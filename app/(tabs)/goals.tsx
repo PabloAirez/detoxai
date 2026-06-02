bfe9d58de2152c91efc88ba93f3463e90981c974
Pablo Santos <pabloadscontato@gmail.com>
2026-06-02 06:39:21 -0300

    feat: Criando a primeira versão das páginas do projeto

---
+++ app/(tabs)/goals.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function GoalsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Metas</Text>
+    </View>
+  );
+}
